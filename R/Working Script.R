54316a98935f9bef85193d613a2e3c2cdccf5936
Brystol Bates <brystol.bates@emory.edu>
2025-08-31 13:27:58 -0400

    recode intake condition, and filter to keep only categories of interest

--- R/Working Script.R
+++ R/Working Script.R
@@ -52,36 +52,20 @@ longbeach <- read_csv(here::here("data", "raw", "longbeach.csv"))
 
  #creating another function to relabel becuase its easier than creating new system
 
- recolor <- function(dataframe_name, input_column_name, new_column_name) {
+ recode_intake <- function(dataframe_name, input_column_name, new_column_name) {
 
- 	color_code <- list(
- 		black  = c("black", "black lynx point", "black smoke", "black tabby", "black tiger"),
- 		blue   = c("blue"),
- 		brown  = c("brown", "brown tabby",	"brown tiger", "ch lynx point", "chocolate",
- 							 "chocolate point", "liver", "red", "red point", "ruddy", "sable"),
- 		tan    = c("buff", "cream", "cream point", "cream tabby", "cream tiger",
- 							 "ct lynx point", "fawn", "flame point", "tan","wheat"),
- 		green  = c("green"),
-
- 		grey   = c("blue cream", "blue lynx point", "blue point", "blue tabby", "gray",
- 							 "grey","gray tabby","gray tiger", "lilac lynx point", "lilac point",
- 							 "lilac_cream point", "lynx point", "point", "seal", "seal point", "silver",
- 							 "silver lynx point", "silver tabby", "st lynx point"),
- 		multi   = c("calico", "calico dilute", "calico point", "calico tabby", "snowshoe",
- 								"torbi", "tortie", "tortie dilute", "tortie point", "tricolor"),
- 		orange   = c("orange","orange tabby", "orange tiger" , "peach"),
- 		pink     = c("pink"),
- 		spotted  = c("dapple", "blue brindle", "blue merle", "blue tick", "brown brindle",
- 								 "brown merle", "liver tick", "red merle", "yellow brindle"),
- 		white    = c("white"),
- 		yellow   = c("apricot", "blonde", "gold", "yellow"),
- 		unknown  = c("unknown", "NA")
+ 	intake_code <- list(
+ 		normal  = c("normal"),
+ 		mild   = c("behavior mild", "ill mild", "injured mild" ),
+ 		moderate  = c("behavior moderate", "ill moderatete", "injured moderate"),
+ 		severe    = c("behavior severe", "ill severe", "injured severe"),
+ 		feral_fractious = c("feral", "fractious")
  	)
 
  	dataframe_name[[new_column_name]] <- "other"
 
- 	for (name in names(color_code)) {
- 		match_rows <- dataframe_name[[input_column_name]] %in% color_code[[name]]
+ 	for (name in names(intake_code)) {
+ 		match_rows <- dataframe_name[[input_column_name]] %in% intake_code[[name]]
  		dataframe_name[[new_column_name]][match_rows] <- name
  	}
 
@@ -92,7 +76,9 @@ longbeach <- read_csv(here::here("data", "raw", "longbeach.csv"))
 #only including cats, datafile is very large
 longbeach_cats <- longbeach |>
 								 filter(animal_type == "cat") |>
-								 recolor("primary_color", "color")
+								 recolor("primary_color", "color") |>
+							 	 recode_intake("intake_condition", "intake") |>
+								 filter(!(intake == "other"))
 
 #gtsummary table
 
@@ -101,11 +87,11 @@ tbl_summary(
 
 	by = outcome_is_dead,
 
-	include = c(color,intake_condition),
+	include = c(color,intake),
 
 label = list(
 	color ~ "Color",
-	intake_condition ~ "Intake Condition"
+	intake ~ "Intake Condition"
 
 ))
 
